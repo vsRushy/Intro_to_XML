7e980772269ac627dd765169a744bdf831d8214b
vsRushy <gerardmarcosfreixasgmail.com>
2018-09-19 14:38:44 +0200

    TODO 3
    Load config.xml file using load_file() method from the xml_document class. If
    everything goes well, load the top tag/element inside the xml_node property
    created in the last TODO

--- Motor2D/j1App.cpp
+++ Motor2D/j1App.cpp
@@ -61,6 +61,15 @@ bool j1App::Awake()
 	// If everything goes well, load the top tag inside the xml_node property
 	// created in the last TODO
 
+	pugi::xml_parse_result result = document.load_file("config.xml");
+	if (result == NULL)
+		LOG("Could not load config.xml. Error %s", result.description());
+	else
+	{
+		LOG("config.xml loaded correctly.");
+		auto top = node.first_child();
+	}
+
 	bool ret = true;
 
 	p2List_item<j1Module*>* item;
